13e96199561d1e5e5c76b48096bae3bca84099e8
wafiqaf <wafiqafarhana@gmail.com>
2025-02-05 18:59:35 -0800

    Added basic trees, garlic, player, movement, planes, walls, and camera that follows the player

---
+++ Assets/Scripts/Garlic.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Garlic : MonoBehaviour
+{
+    [SerializeField]
+    public Vector3 targetPos1;
+    [SerializeField]
+    public Vector3 targetPos2;
+    [SerializeField]
+    public float speed;
+    public bool canMove;
+    public bool firstMove;
+    void Start()
+    {
+
+        firstMove = true;
+
+    }
+    void Update()
+    {
+        if (transform.position == targetPos1)
+        {
+            firstMove = false;
+        }
+        if (transform.position == targetPos2)
+        {
+            firstMove = true;
+        }
+        if (canMove)
+        {
+            if (firstMove)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPos1, speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPos2, speed * Time.deltaTime);
+            }
+        }
+
+    }
+}
