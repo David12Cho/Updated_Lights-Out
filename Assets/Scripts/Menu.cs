0880f47b0350d6c097425b0fbcdf28b2a759ed33
va-wei <vawei@calpoly.edu>
2025-02-12 21:40:08 -0800

    add controls window script + ui

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,8 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject controlsWindow; // parent gameobj for controls window
+    public GameObject menuElements; // title + buttons
     public void OnNewGameButton() 
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OnControlsButton() // when "controls" button is clicked
+    {
+        controlsWindow.SetActive(true);
+        menuElements.SetActive(false);
+    }
+
+    public void OnCloseControls() // when closing the window
+    {
+        controlsWindow.SetActive(false);
+        menuElements.SetActive(true);
     }
 }
