b84fec9ac32378cafca81b98e7c3db010ba45658
ashfong <afong42@calpoly.edu>
2025-02-15 16:25:44 -0800

    Health Display - garlic only
    Health display UI. Loses life when garlic is touched

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -86,4 +86,13 @@ public class PlayerController : MonoBehaviour
             GameManager.instance.UpdateHealth(1);
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Garlic"))
+        {
+            Debug.Log("Collided with Garlic! (Physical Collision)");
+            FindObjectOfType<HealthDisplay>().LoseLife();
+        }
+    }
 }
