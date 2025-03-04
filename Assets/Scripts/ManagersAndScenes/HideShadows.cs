052f913c2883b8e66c6f0038c0ba6a5fe65c7e4e
va-wei <vawei@calpoly.edu>
2025-03-04 13:01:09 -0800

    require crouching for small box

--- Assets/Scripts/ManagersAndScenes/HideShadows.cs
+++ Assets/Scripts/ManagersAndScenes/HideShadows.cs
@@ -5,8 +5,18 @@ public class HideShadows : MonoBehaviour
     void Start()
     {
         GameObject[] shadowObjects = GameObject.FindGameObjectsWithTag("Shadow");
+        GameObject[] crouchObjects = GameObject.FindGameObjectsWithTag("NotCrouched");
 
         foreach (GameObject obj in shadowObjects)
+        {
+            MeshRenderer meshRenderer = obj.GetComponent<MeshRenderer>();
+            if (meshRenderer != null)
+            {
+                meshRenderer.enabled = false;  // Make invisible
+            }
+        }
+
+        foreach (GameObject obj in crouchObjects)
         {
             MeshRenderer meshRenderer = obj.GetComponent<MeshRenderer>();
             if (meshRenderer != null)
